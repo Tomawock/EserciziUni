5602c2576306e9fb8a1429f52e30b2110f2ec32b
Tomawock <lori.tomasetti@hotmail.it>
2016-03-09 15:57:33 +0100

    Test
    Test

---
+++ TestRep/src/it/ing/unibs/fondamneti/testrep/Rep.java
@@ -0,0 +1,11 @@
+package it.ing.unibs.fondamneti.testrep;
+
+public class Rep {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("SONO UN REPOSTORY WOW");
+	}
+
+}
