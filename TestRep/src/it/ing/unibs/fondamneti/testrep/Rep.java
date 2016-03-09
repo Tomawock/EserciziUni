a6146b86a079ec610eea837dc4f923efb0d23fbf
Tomawock <lori.tomasetti@hotmail.it>
2016-03-09 16:02:29 +0100

    Ho introdotto un nuovo attributo

--- TestRep/src/it/ing/unibs/fondamneti/testrep/Rep.java
+++ TestRep/src/it/ing/unibs/fondamneti/testrep/Rep.java
@@ -1,6 +1,19 @@
 package it.ing.unibs.fondamneti.testrep;
 
-public class Rep {
+public class Rep 
+{
+	
+	private int x;
+		
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+
 
 	public static void main(String[] args) 
 	{
